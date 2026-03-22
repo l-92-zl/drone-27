62316cc673debf689a71d0d67a5b0b2f154bd692
l-92-zl <2183750292@qq.com>
2026-03-22 21:44:08 +0800

    Update WebSocketConfig.java

--- 27/src/main/java/myproject/config/WebSocketConfig.java
+++ 27/src/main/java/myproject/config/WebSocketConfig.java
@@ -2,27 +2,37 @@ package myproject.config;
 
 import myproject.handle.ResultHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+import java.time.Duration;
+
 @Configuration
-@EnableWebSocket// 开启 WebSocket 功能
+@EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
     @Autowired
-    private ResultHandler resultHandler;// 注入自定义的处理器
+    private ResultHandler resultHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // 把处理器绑定到 /ws/{taskId}
-        // setAllowedOrigins("*") 允许前端接口到 /ws/{taskId}
         registry.addHandler(resultHandler, "/ws/{taskId}")
-                .setAllowedOrigins("*");
+                .setAllowedOriginPatterns("*"); // 关键修复：使用新API
+    }
+    @Bean
+        public RestTemplate restTemplate() {
+            SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+            factory.setConnectTimeout((int) Duration.ofSeconds(3).toMillis()); // 连接超时3秒
+            factory.setReadTimeout((int) Duration.ofSeconds(5).toMillis());    // 读取超时5秒
+            return new RestTemplate(factory);
+
     }
 }
-
 
 
 
