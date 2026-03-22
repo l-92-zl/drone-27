bd76dfc2ab58b3fabd3337e2e85f5963ccf18cbb
l-92-zl <2183750292@qq.com>
2026-03-22 21:45:56 +0800

    Update Application.java

--- 27/src/main/java/myproject/Application.java
+++ 27/src/main/java/myproject/Application.java
@@ -2,11 +2,12 @@ package myproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
 @EnableWebSocket // 开启 WebSocket 支持
-
+@EnableAsync
 
 public class Application {
     public static void main(String[] args) {
